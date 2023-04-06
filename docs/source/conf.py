b87079e66936a5922b578513134a071b36e3045f
Abby Moore <aemoore62@gmail.com>
2023-04-05 22:25:40 -0400

    docs: update project

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'sSquared'
+copyright = '2023, Abigail Elizabeth'
+author = 'Abigail Elizabeth'
 
 release = '0.1'
 version = '0.1.0'
